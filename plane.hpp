0c3ee726dd1554966e37689ed800c9d63e6195f6
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-19 17:20:54 -0500

    added sizes for control surfaces

--- plane.hpp
+++ plane.hpp
@@ -6,11 +6,24 @@ class plane {
   public:
     plane();
     ~plane();
+    void bank(float degrees) {
+      
+    }
   private:
     float angleOfAttack;
     float bankAngle;
     float airspeed;
     float altitude;
+  
+    float aileronSize;
+    float elevatorSize;
+    float rudderSize;
+    
+  
+    float aileronAngle;
+    float elevatorAngle;
+    float rudderAngle;
+    
     point location;
 };
 #endif
