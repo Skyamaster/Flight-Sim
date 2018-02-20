5e91fac42fa5e3a3ba15909ef2df14c58d7b5297
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-20 16:43:22 -0500

    impoved shapes

--- graphics.cpp
+++ graphics.cpp
@@ -1,13 +1,20 @@
 #include "graphics.hpp"
 
+color::color(int a, b, c) {
+  r = a;
+  g = b;
+  b = c;
+}
+
+point::point(int a, int b) {
+  x = a;
+  y = b;
+}
 double point::distance(point p) {
-  return sqrt((x()-p.x())*(x()-p.x())+(y()-p.y())*(y()-p.y()));
+  return sqrt((x-p.x)*(x-p.x)+(y-p.y)*(y-p.y));
 }
 
-double point::x() {
-  return this->x;
-}
-
-double point::y() {
-  return this->y;
+rectangle::rectangle(point a, point b) {
+  tl = a;
+  br = b;
 }
