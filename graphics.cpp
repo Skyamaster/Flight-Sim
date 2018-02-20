35f56fb940a1d9a32ad6ed0450e86b5e5a9d9f6c
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-20 10:08:39 -0500

    added distance for shapes

--- graphics.cpp
+++ graphics.cpp
@@ -1,1 +1,13 @@
 #include "graphics.hpp"
+
+double point::distance(point p) {
+  return sqrt((x()-p.x())*(x()-p.x())+(y()-p.y())*(y()-p.y()));
+}
+
+double point::x() {
+  return this->x;
+}
+
+double point::y() {
+  return this->y;
+}
