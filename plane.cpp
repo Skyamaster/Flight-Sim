43affd27307362d08c7acecd46b842882aee8264
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-19 21:23:26 -0500

    filled in constructor

--- plane.cpp
+++ plane.cpp
@@ -1,1 +1,19 @@
 #include "plane.hpp"
+
+void plane::plane(image i, point l, float aSize, float eSize, float rSize, float cSection, float rcSection = 0) {
+  looks = i; // I could've used an initialization list for this...
+  
+  angleOfAttack = 0;
+  bankAngle = 0;
+  airspeed = 0;
+  altitude = 0;
+  aileronSize = aSize;
+  elevatorSize = eSize;
+  rudderSize = rSize;
+  aileronAngle = 0;
+  elevatorAngle = 0;
+  rudderAngle = 0;
+  crossSection = cSection;
+  radarCrossSection = ((rcSection == 0) ? (cSection) : (rcSection));
+  location = l;
+}
