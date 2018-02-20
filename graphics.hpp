cf071cc8a86c10717b0680e8f8cf436314de9677
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-20 10:08:28 -0500

    added shapes

--- graphics.hpp
+++ graphics.hpp
@@ -5,19 +5,49 @@
 
 class point {
   public:
-    point();
+    point(int, int);
     ~point();
-    int distance(point);
+    double distance(point);
+    double x();
+    double y();
   private:
     double x;
     double y;
 };
+
+class rectangle {
+  public:
+    rectangle(point, point);
+    ~rectangle();
+  private:
+};
+
+class line {
+  public:
+    line(point, point);
+    ~line();
+  private:
+};
+
+class circle {
+  public:
+    circle(point, int);
+    ~circle();
+  private:
+};
+
+class polygon {
+  public:
+    polygon(vector<point>);
+    ~polygon;
+  private:
+}
 
 class image {
   public:
     image();
     ~image();
   private:
-}
+};
 
 #endif
