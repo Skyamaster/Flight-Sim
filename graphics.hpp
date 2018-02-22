1751ab17a35255a65b429f3f526394a4c904e868
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-22 13:40:02 -0500

    switched to references and added more copy constructors

--- graphics.hpp
+++ graphics.hpp
@@ -16,6 +16,7 @@ class point {
 class color {
   public:
     color(int, int, int);
+    color(const color& c);
     ~color();
     int r;
     int g;
@@ -26,41 +27,47 @@ class shape {
   public:
     shape();
     ~shape();
+    shape(const shape& s);
     color fillColor;
 };
 
 class rectangle : shape {
   public:
-    rectangle(point, point);
+    rectangle(const point&, const point&);
     ~rectangle();
+    rectangle(const rectangle& r);
     point tl;
     point br;
 };
 
 class line : shape {
   public:
-    line(point, point);
+    line(const point&, const point&);
     ~line();
+    line(const line& l);
     point start;
     point end;
 };
 
 class circle : shape {
   public:
-    circle(point&, int);
+    circle(const circle& c);
+    circle(const point&, int);
     ~circle();
 };
 
 class polygon : shape {
   public:
     polygon();
-    polygon(vector<point>);
+    polygon(const vector<point>&);
+    polygon(const polygon& p);
     ~polygon();
 };
 
 class image {
   public:
     image();
+    image(const image& i);
     ~image();
 };
 
