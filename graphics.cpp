affa23a189ab74f3b556280898355578acf9b979
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-22 13:42:14 -0500

    added copy constructors

--- graphics.cpp
+++ graphics.cpp
@@ -6,6 +6,11 @@ color::color(int a, int b, int c) {
   g = b;
   b = c;
 }
+color::color(const color& c) {
+  r = c.r;
+  g = c.g;
+  b = c.b;
+}
 
 point::point(const point& p) {
   x = p.x;
@@ -22,6 +27,10 @@ double point::distance(const point& p) {
 rectangle::rectangle(const point& a, const point& b) {
   tl = a;
   br = b;
+}
+rectangle::rectangle(const rectangle& r) {
+  tl = r.tl;
+  br = r.bl;
 }
 
 polygon::polygon(const vector<point>& p) {
