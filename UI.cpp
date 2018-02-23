dcd994a6afdee9dbdf318cb2582fc0a18530f9d1
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-23 09:55:31 -0500

    added draw methods

--- UI.cpp
+++ UI.cpp
@@ -1,18 +1,23 @@
 #include "UI.hpp"
 
+using namespace UIHandlerInternal;
+
 UIHandler::UIHandler() {}
 UIHandler::UIHandler(bool m) : mode(m) {}
 UIHandler::draw(const line& l) {
+  renderLine(l.start.x, l.start.y, l.end.x, l.end.y);
 }
 UIHandler::draw(const rectangle& r) {
+  point topLeft = r.tl;
+  point bottomRight = r.br;
+  int width = bottomRight.x - topLeft.x;
+  int height = topLeft.y - bottomRight.y;
 }
 void UIHandler::draw(const polygon& p) {
-  vector<line> lines;
   if (p.points.size() < 2) {
     return; //error - polygon must have at least 2 points
   }
   for (int j = 0; j < p.points.size()-1; j++) {
-    line l(p.points[j], p.points[j+1]);
-    lines.push_back(l);
+    renderLine(p.points[j].x, p.points[j].y, p.points[j+1].x, p.points[j+1].y);
   }
 }
