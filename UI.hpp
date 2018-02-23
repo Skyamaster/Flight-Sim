6d6e278abb77c8c438e697289fe9bd1bf941d101
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-23 07:26:08 -0500

    improved UI skeleton

--- UI.hpp
+++ UI.hpp
@@ -2,11 +2,19 @@
 #define UIHPP
 
 #include "include.hpp"
+#include "graphics.hpp"
 
 class UIHandler {
   public:
     UIHandler();
+    UIHandler(bool mode);
     ~UIHandler();
+    
+    // UI creation functions
+    void draw(const line& l);
+    void draw(const rectangle& r);
+    void draw(const polygon& p);
   private:
+    bool mode;
 };
 #endif
