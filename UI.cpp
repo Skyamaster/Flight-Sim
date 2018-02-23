9bd9212220cda7758edf1abdc0a0f31be4f0a771
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-23 09:25:13 -0500

    added line extraction code to drawLine

--- UI.cpp
+++ UI.cpp
@@ -1,1 +1,18 @@
 #include "UI.hpp"
+
+UIHandler::UIHandler() {}
+UIHandler::UIHandler(bool m) : mode(m) {}
+UIHandler::draw(const line& l) {
+}
+UIHandler::draw(const rectangle& r) {
+}
+void UIHandler::draw(const polygon& p) {
+  vector<line> lines;
+  if (p.points.size() < 2) {
+    return; //error - polygon must have at least 2 points
+  }
+  for (int j = 0; j < p.points.size()-1; j++) {
+    line l(p.points[j], p.points[j+1]);
+    lines.push_back(l);
+  }
+}
