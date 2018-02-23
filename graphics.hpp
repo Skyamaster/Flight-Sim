ada7e85dcf5126b40e8516bb43e25208ae71e71e
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-23 09:55:14 -0500

    added fillcolor

--- graphics.hpp
+++ graphics.hpp
@@ -3,6 +3,8 @@
 
 #include "include.hpp"
 
+color black(0, 0, 0);
+
 class point {
   public:
     point();
@@ -32,49 +34,56 @@ class shape {
 class rectangle : shape {
   public:
     rectangle();
-    rectangle(const point&, const point&);
+    rectangle(const point&, const point&, const color& fc = black);
     rectangle(const rectangle& r);
     ~rectangle();
     point tl;
     point br;
+    color fillColor;
 };
 
 class line : shape {
   public:
     line();
-    line(const point&, const point&);
+    line(const point&, const point&, const color& fc = black);
     line(const line& l);
     ~line();
     point start;
     point end;
+    color fillColor;
 };
 
 class curve : shape {
   public:
     curve();
-    curve(const point&, const point&, int);
+    curve(const point&, const point&, int, const color& fc = black);
     curve(const cure& c);
     ~curve();
     point start;
     point end;
     int curvature; // a line is a curve of curvature 0 - shows the maximum distance between the line and the curve
+    color fillColor;
 }
 
 class circle : shape {
   public:
     circle();
     circle(const circle& c);
-    circle(const point&, int);
+    circle(const point&, int, const color& fc = black);
     ~circle();
+    point center;
+    int radius;
+    color fillColor;
 };
 
 class polygon : shape {
   public:
     polygon();
-    polygon(const vector<point>&);
+    polygon(const vector<point>&, const color& fc = black);
     polygon(const polygon& p);
     ~polygon();
     vector<point> points;
+    color fillColor;
 };
 
 class image {
