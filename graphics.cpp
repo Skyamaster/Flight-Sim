6972b1e369d46be5c4d430c23479865dacff815a
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-26 09:50:22 -0500

    added more vector helper functions

--- graphics.cpp
+++ graphics.cpp
@@ -4,3 +4,30 @@
 double point::distance(const point& p) {
   return sqrt((x-p.x)*(x-p.x)+(y-p.y)*(y-p.y));
 }
+
+Vector Vector::operator+(const vector& v) {
+  return Vector(i + v.i, j + v.j, k + v.k);
+}
+Vector& Vector::operator+=(const Vector& v) {
+  i += v.i;
+  j += v.j;
+  k += v.k;
+  return *this;
+}
+Vector Vector::operator-(const vector& v) {
+  return Vector(i - v.i, j - v.j, k - v.k);
+}
+Vector& Vector::operator-=(const Vector& v) {
+  i -= v.i;
+  j -= v.j;
+  k -= v.k;
+  return *this;
+}
+Vector Vector::operator-() {
+  return Vector(-i, -j, -k);
+}
+
+double Vector::dot(const Vector& v) {
+  return i * v.i + j * v.j + k * v.k;
+}
+
