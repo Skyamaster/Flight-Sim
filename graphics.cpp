1a159c7fec3c840b4c3f25361890012f8b2f95b8
Ndrew Xie <ndrew.xie@gmail.com>
2018-02-27 12:49:31 -0500

    improved graphics helper functions

--- graphics.cpp
+++ graphics.cpp
@@ -26,8 +26,20 @@ Vector& Vector::operator-=(const Vector& v) {
 Vector Vector::operator-() {
   return Vector(-i, -j, -k);
 }
-
 double Vector::dot(const Vector& v) {
   return i * v.i + j * v.j + k * v.k;
 }
+Vector Vector::cross(const Vector& v) {
+  return Vector(j * v.k - k * v.j, k * v.i - i * v.k, i * v.j - j * v.i);
+}
+double Vector::magnitude() {
+  return sqrt(i*i + j*j + k*k);
+}
 
+Plane::Plane(Point a, Point b, Point c) {
+  Vector n = (b - a).cross(c - a);
+  a = n.i;
+  b = n.j;
+  c = n.k;
+  d = -(Vector(p1).dot(n));
+}
