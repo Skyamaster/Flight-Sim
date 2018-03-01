241d7cc4804785a690184714e8bd5a92f01d569b
Ndrew Xie <ndrew.xie@gmail.com>
2018-03-01 09:01:44 -0500

    fixed constructor

--- plane.cpp
+++ plane.cpp
@@ -1,6 +1,6 @@
 #include "plane.hpp"
 
-plane::plane(image i, point l, float aSize, float eSize, float rSize, float cSection, float rcSection = 0) : looks(i), angleOfAttack(0), bankAngle(0), altitude(0), aileronSize(0), elevatorSize(0), rudderSize(0), aileronAngle(0), elevatorAngle(0), rudderAngle(0), crosSection(cSection), radarCrossSection(((rcSection == 0) ? (cSection) : (rcSection)), location(l) {}
+plane::plane(Wireframe i, Point l, float aSize, float eSize, float rSize, float cSection, float rcSection = 0) : looks(i), angleOfAttack(0), bankAngle(0), altitude(0), aileronSize(aSize), elevatorSize(eSize), rudderSize(rSize), aileronAngle(0), elevatorAngle(0), rudderAngle(0), crosSection(cSection), radarCrossSection(((rcSection == 0) ? (cSection) : (rcSection)), location(l) {} {}
 
 void plane::recalculate() {
 }
