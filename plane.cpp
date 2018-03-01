e2b769c401f59f8ce8d6c4f929619cf47cad0bc2
Ndrew Xie <ndrew.xie@gmail.com>
2018-03-01 09:46:19 -0500

    improved constructor

--- plane.cpp
+++ plane.cpp
@@ -1,6 +1,6 @@
 #include "plane.hpp"
 
-plane::plane(Wireframe i, Point l, float aSize, float eSize, float rSize, float cSection, float rcSection = 0) : looks(i), angleOfAttack(0), bankAngle(0), altitude(0), aileronSize(aSize), elevatorSize(eSize), rudderSize(rSize), aileronAngle(0), elevatorAngle(0), rudderAngle(0), crosSection(cSection), radarCrossSection(((rcSection == 0) ? (cSection) : (rcSection)), location(l) {} {}
-
+plane::plane(Wireframe i, Point l, float maxPower, float aSize, float eSize, float rSize, float cSection, float rcSection = 0) : looks(i), angleOfAttack(0), thrustPercent(0), maxThrust(maxPower), bankAngle(0), altitude(0), aileronSize(aSize), elevatorSize(eSize), rudderSize(rSize), aileronAngle(0), elevatorAngle(0), rudderAngle(0), crosSection(cSection), radarCrossSection(((rcSection == 0) ? (cSection) : (rcSection)), location(l) {};
 void plane::recalculate() {
+  
 }
