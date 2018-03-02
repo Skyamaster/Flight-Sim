0e5832f5485d2b8a309524e1afd7d0e2f3ccbcde
Ndrew Xie <ndrew.xie@gmail.com>
2018-03-02 07:26:36 -0500

    computed new motion vector for each simulation cycle

--- plane.cpp
+++ plane.cpp
@@ -1,6 +1,7 @@
 #include "plane.hpp"
 
 plane::plane(Wireframe i, Point l, float maxPower, float aSize, float eSize, float rSize, float cSection, float rcSection = 0) : looks(i), angleOfAttack(0), thrustPercent(0), maxThrust(maxPower), bankAngle(0), altitude(0), aileronSize(aSize), elevatorSize(eSize), rudderSize(rSize), aileronAngle(0), elevatorAngle(0), rudderAngle(0), crosSection(cSection), radarCrossSection(((rcSection == 0) ? (cSection) : (rcSection)), location(l) {};
-void plane::recalculate() {
+void plane::recalculate() { 
+  motion = (thrust.magnitude() * thrust + surfaces.magnitude() * surfaces + motion.magnitude() * motion) / (thrust.magnitude() + surfaces.magnitude() + motion.magnitude());
   
 }
